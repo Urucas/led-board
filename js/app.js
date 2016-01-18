516979b499cc1eb434075450c164e8147fbe7f85
vrunoa <vruno.alassia@gmail.com>
2016-01-18 12:37:32 -0800

    add base html files

---
+++ js/app.js
@@ -0,0 +1,14 @@
+angular.module("LedsSimulatorApp", [])
+.controller("LedsController", ["$scope", function($scope) {
+  var set = [[1,0,0], [0,1,0], [0,1,0], [0,0,1]]
+  $scope.sets = [set]
+  $scope.isLedOn = function(set, row, col) {
+    var state = $scope.sets[set][row][col]
+    return state == 1 ? "led-on" : "led-off"
+  }
+  $scope.toggleLed = function(set, row, col) {
+    var state = $scope.sets[set][row][col]
+    $scope.sets[set][row][col] = state == 1 ? 0 : 1
+  }
+
+}])
