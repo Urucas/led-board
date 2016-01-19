a260d2890944d72b33acc2749fbf35f88cfd0952
vrunoa <vruno.alassia@gmail.com>
2016-01-18 18:03:54 -0800

    add form

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
 angular.module("LedsSimulatorApp", [])
 .controller("LedsController", ["$scope", function($scope) {
-  var set = [[1,0,0], [0,1,0], [0,1,0], [0,0,1]]
-  $scope.sets = [set]
+  $scope.setsCant = 1
+  $scope.rowsCant = 4
+  $scope.colsCant = 3
+  $scope.createSet = function(rows, cols) {
+    var set = [];
+    var row = []
+    for(i=0;i<rows;i++) {
+      var col = []
+      for(j=0;j<cols; j++) {
+        col.push(0)
+      }
+      row.push(col)
+    }
+    set.push(row)
+    return set
+  }
+
+  $scope.sets = $scope.createSet($scope.rowsCant, $scope.colsCant)
   $scope.isLedOn = function(set, row, col) {
     var state = $scope.sets[set][row][col]
     return state == 1 ? "led-on" : "led-off"
@@ -9,6 +25,10 @@ angular.module("LedsSimulatorApp", [])
   $scope.toggleLed = function(set, row, col) {
     var state = $scope.sets[set][row][col]
     $scope.sets[set][row][col] = state == 1 ? 0 : 1
+  }
+
+  $scope.updateBoard = function() {
+    $scope.sets = $scope.createSet($scope.rowsCant, $scope.colsCant)
   }
   
 }])
